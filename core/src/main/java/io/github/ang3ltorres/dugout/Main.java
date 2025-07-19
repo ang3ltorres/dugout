a72887a862204915129d70170a0a3bad1bd75e26
ang3ltorres <angel.storres@alumnos.udg.mx>
2025-07-19 14:09:27 -0600

    setting up coordinate system

--- core/src/main/java/io/github/ang3ltorres/dugout/Main.java
+++ core/src/main/java/io/github/ang3ltorres/dugout/Main.java
@@ -1,34 +1,69 @@
 package io.github.ang3ltorres.dugout;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+public class Main extends ApplicationAdapter
+{
+  private SpriteBatch batch;
 
-    @Override
-    public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
-    }
+  private Texture imageTexture;
+  private TextureRegion image;
 
-    @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
-    }
+  private OrthographicCamera mainCamera;
+  private OrthographicCamera bufferCamera;
 
-    @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
-    }
+  private FrameBuffer frameBuffer;
+  private TextureRegion bufferRegion;
+
+  @Override
+  public void create()
+  {
+    batch = new SpriteBatch();
+
+    imageTexture = new Texture("libgdx.png");
+    imageTexture.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
+    image = new TextureRegion(imageTexture);
+    image.flip(false, true);
+
+    frameBuffer = new FrameBuffer(Pixmap.Format.RGBA8888, 800, 480, false);
+    bufferRegion = new TextureRegion(frameBuffer.getColorBufferTexture());
+    bufferRegion.flip(false, true);
+
+    mainCamera = new OrthographicCamera(1280, 720);
+    mainCamera.setToOrtho(true);
+
+    bufferCamera = new OrthographicCamera(800, 480);
+    bufferCamera.setToOrtho(false);
+  }
+
+  @Override
+  public void render()
+  {
+    frameBuffer.begin();
+    ScreenUtils.clear(0f, 0f, 0f, 1f);
+    batch.setProjectionMatrix(bufferCamera.combined);
+    batch.begin();
+    batch.draw(image, 0, 0);
+    batch.end();
+    frameBuffer.end();
+
+    ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+    batch.setProjectionMatrix(mainCamera.combined);
+    batch.begin();
+    batch.draw(bufferRegion, 0, 0);
+    batch.end();
+  }
+
+  @Override
+  public void dispose()
+  {
+    batch.dispose();
+    imageTexture.dispose();
+    frameBuffer.dispose();
+  }
 }
--- lwjgl3/src/main/java/io/github/ang3ltorres/dugout/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/io/github/ang3ltorres/dugout/lwjgl3/Lwjgl3Launcher.java
@@ -1,37 +1,51 @@
 package io.github.ang3ltorres.dugout.lwjgl3;
 
+import com.badlogic.gdx.Version;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3NativesLoader;
+
 import io.github.ang3ltorres.dugout.Main;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
-    public static void main(String[] args) {
-        if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
-        createApplication();
+  public static void main(String[] args)
+  {
+    String osName = System.getProperty("os.name").toLowerCase();
+
+    if (osName.contains("windows"))
+    {
+      String programData = System.getenv("ProgramData");
+      
+      if (programData == null)
+        programData = "C:\\Temp\\";
+
+      String prevTmpDir = System.getProperty("java.io.tmpdir", programData);
+      String prevUser = System.getProperty("user.name", "libGDX_User");
+      System.setProperty("java.io.tmpdir", programData + "/libGDX-temp");
+      System.setProperty("user.name", ("User_" + prevUser.hashCode() + "_GDX" + Version.VERSION).replace('.', '_'));
+      Lwjgl3NativesLoader.load();
+      System.setProperty("java.io.tmpdir", prevTmpDir);
+      System.setProperty("user.name", prevUser);
     }
 
-    private static Lwjgl3Application createApplication() {
-        return new Lwjgl3Application(new Main(), getDefaultConfiguration());
-    }
+    createApplication();
+  }
 
-    private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
-        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
-        configuration.setTitle("dugout");
-        //// Vsync limits the frames per second to what your hardware can display, and helps eliminate
-        //// screen tearing. This setting doesn't always work on Linux, so the line after is a safeguard.
-        configuration.useVsync(true);
-        //// Limits FPS to the refresh rate of the currently active monitor, plus 1 to try to match fractional
-        //// refresh rates. The Vsync setting above should limit the actual FPS to match the monitor.
-        configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
-        //// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
-        //// useful for testing performance, but can also be very stressful to some hardware.
-        //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
+  private static Lwjgl3Application createApplication()
+  {
+    return new Lwjgl3Application(new Main(), getDefaultConfiguration());
+  }
 
-        configuration.setWindowedMode(640, 480);
-        //// You can change these files; they are in lwjgl3/src/main/resources/ .
-        //// They can also be loaded from the root of assets/ .
-        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
-        return configuration;
-    }
+  private static Lwjgl3ApplicationConfiguration getDefaultConfiguration()
+  {
+    Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+    configuration.setTitle("dugout");
+    configuration.useVsync(false);
+    configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
+    configuration.setWindowedMode(1280, 720);
+    configuration.setDecorated(false);
+    configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+    return configuration;
+  }
 }
